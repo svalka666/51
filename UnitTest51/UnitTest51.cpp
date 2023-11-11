78f20bb454cc3bb782b376059226b4dc32dac84c
svalka <angeddonia@gmail.com>
2023-11-11 22:40:10 +0200

    Добавьте файлы проекта.

---
+++ UnitTest51/UnitTest51.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../51/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest51
+{
+	TEST_CLASS(UnitTest51)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double p;
+			p = h(2, 3);
+			Assert::AreEqual(p, -5, 4);
+		}
+	};
+}
